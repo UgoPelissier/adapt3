ab019bf6dae4dd5587e431803bf0a5e143e46e65
UgoPelissier <upelissier@gmail.com>
2023-10-16 13:22:50 +0200

    ✨ Adapt script to metric adaptation prediction

--- adapt.sh
+++ adapt.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 clear
 
-python="/mnt/host/c/Anaconda3/python.exe"
-freefem="/mnt/host/c/Users/ugo.pelissier/FreeFem++/FreeFem++.exe"
-gmsh="/mnt/host/c/Users/ugo.pelissier/gmsh/gmsh.exe"
-mmg3d="/mnt/host/c/Users/ugo.pelissier/mmg/mmg3d_O3.exe"
-
 if [ "$1" == "-h" ]; then
     echo "Usage: ./adapt.sh -d <data>"
     echo "data: number of the data to adapt"
@@ -40,13 +35,10 @@ mkdir -p $data_dir/adapt/
 rm src/adapt.log
 
 echo "Generating geo file (python)"
-$python src/geo.py -d $data >> src/adapt.log
+python src/geo.py -d $data >> src/adapt.log
 
 echo "Converting msh (gmsh) to mesh (INRIA)"
-$gmsh src/convert.geo - >> src/adapt.log
-
-echo "Computing adaptation metric (FreeFem++)"
-$freefem src/metric.edp >> src/adapt.log
+gmsh src/convert.geo - >> src/adapt.log
 
 rm src/data.txt
 rm src/step.txt
@@ -54,12 +46,12 @@ rm src/step.txt
 if [ '$step' = 'test' ]
 then
     echo "Adapting mesh (MMG) to real solution"
-    $mmg3d $data_dir/mesh/cad_$data.mesh -sol $data_dir/sol/cad_$data.sol >> src/adapt.log
+    mmg3d_O3 $data_dir/mesh/cad_$data.mesh -sol $data_dir/sol/cad_$data.sol >> src/adapt.log
     mv $data_dir/mesh/cad_$data.o.mesh $data_dir/adapt/cad_${data}.o.mesh
 fi
 
 echo "Adapting mesh (MMG) to predicted solution"
-$mmg3d $data_dir/mesh/cad_$data.mesh -sol $data_dir/sol/cad_${data}_pred.sol >> src/adapt.log
+mmg3d_O3 $data_dir/mesh/cad_$data.mesh -sol $data_dir/sol/cad_${data}_pred.sol >> src/adapt.log
 mv $data_dir/mesh/cad_$data.o.mesh $data_dir/adapt/cad_${data}_pred.o.mesh
 
 rm $data_dir/mesh/cad_$data.o.sol
